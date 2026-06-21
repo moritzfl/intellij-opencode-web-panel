897470f7d374af69c1ca414ef3a373fb56802787
moritz <me@moritzf.de>
2026-06-21 08:35:38 +0200

    refactor: extract browser request handler

---
+++ src/main/kotlin/de/moritzf/opencodewebpanel/toolWindow/OpenCodeBrowserRequestHandler.kt
@@ -0,0 +1,75 @@
+package de.moritzf.opencodewebpanel.toolWindow
+
+import de.moritzf.opencodewebpanel.settings.OpenCodeSettingsState
+import org.cef.browser.CefBrowser
+import org.cef.browser.CefFrame
+import org.cef.callback.CefAuthCallback
+import org.cef.handler.CefResourceRequestHandler
+import org.cef.handler.CefResourceRequestHandlerAdapter
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.misc.BoolRef
+import org.cef.network.CefRequest
+
+internal class OpenCodeBrowserRequestHandler(
+    private val serverManager: SharedOpenCodeServerManager,
+    private val ideNavigation: OpenCodeIdeNavigation,
+) : CefRequestHandlerAdapter() {
+    private val resourceRequestHandler = object : CefResourceRequestHandlerAdapter() {
+        override fun onBeforeResourceLoad(browser: CefBrowser?, frame: CefFrame?, request: CefRequest?): Boolean {
+            val password = serverManager.getServerPassword() ?: return false
+            val requestUrl = request?.url ?: return false
+            if (serverManager.getServerProcess()?.isAlive == true &&
+                OpenCodeServerProtocol.shouldSendBasicAuthHeader(serverManager.getServerUrl(), requestUrl)
+            ) {
+                request.setHeaderByName("Authorization", OpenCodeServerProtocol.buildBasicAuthHeader(password), true)
+            }
+            return false
+        }
+    }
+
+    override fun onBeforeBrowse(
+        browser: CefBrowser?,
+        frame: CefFrame?,
+        request: CefRequest?,
+        userGesture: Boolean,
+        isRedirect: Boolean,
+    ): Boolean {
+        val requestUrl = request?.url ?: return false
+        if (!OpenCodeServerProtocol.isOpenFileLinkRequest(requestUrl)) return false
+        if (OpenCodeSettingsState.getInstance().openFileLinksInIde) {
+            ideNavigation.openFileLinkInIde(OpenCodeServerProtocol.openFileLinkHref(requestUrl))
+        }
+        return true
+    }
+
+    override fun getResourceRequestHandler(
+        browser: CefBrowser?,
+        frame: CefFrame?,
+        request: CefRequest?,
+        isNavigation: Boolean,
+        isDownload: Boolean,
+        requestInitiator: String?,
+        disableDefaultHandling: BoolRef?,
+    ): CefResourceRequestHandler {
+        return resourceRequestHandler
+    }
+
+    override fun getAuthCredentials(
+        browser: CefBrowser?,
+        originUrl: String?,
+        isProxy: Boolean,
+        host: String?,
+        port: Int,
+        realm: String?,
+        scheme: String?,
+        callback: CefAuthCallback?,
+    ): Boolean {
+        val password = serverManager.getServerPassword() ?: return false
+        if (serverManager.getServerProcess()?.isAlive != true) return false
+        if (!OpenCodeServerProtocol.shouldHandleBasicAuthChallenge(serverManager.getServerUrl(), isProxy, host, port)) {
+            return false
+        }
+        callback?.Continue(OpenCodeServerProtocol.BASIC_AUTH_USERNAME, password)
+        return callback != null
+    }
+}
--- src/main/kotlin/de/moritzf/opencodewebpanel/toolWindow/OpenCodeWebToolWindowContent.kt
+++ src/main/kotlin/de/moritzf/opencodewebpanel/toolWindow/OpenCodeWebToolWindowContent.kt
@@ -21,12 +21,7 @@ import de.moritzf.opencodewebpanel.settings.OpenCodeSettingsListener
 import de.moritzf.opencodewebpanel.settings.OpenCodeSettingsState
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
-import org.cef.callback.CefAuthCallback
-import org.cef.handler.CefResourceRequestHandler
-import org.cef.handler.CefResourceRequestHandlerAdapter
 import org.cef.handler.CefLoadHandlerAdapter
-import org.cef.handler.CefRequestHandlerAdapter
-import org.cef.misc.BoolRef
 import org.cef.network.CefRequest
 import javax.swing.JButton
 
@@ -64,6 +59,7 @@ class OpenCodeWebToolWindowContent(private val toolWindow: ToolWindow) : Disposa
             syncCallback = { openCodeLocalStorageQuery.inject("payload") },
         )
         private val systemNotifications = OpenCodeSystemNotifications(project, toolWindow, browser, serverManager)
+        private val requestHandler = OpenCodeBrowserRequestHandler(serverManager, ideNavigation)
         private val openProjectAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD, this)
         private var openProjectScriptScheduled = false
         private var fileLinkScriptScheduled = false
@@ -75,65 +71,6 @@ class OpenCodeWebToolWindowContent(private val toolWindow: ToolWindow) : Disposa
         private var systemNotificationBridgeScriptScheduled = false
         @Volatile
         private var disposed = false
-        private val resourceRequestHandler = object : CefResourceRequestHandlerAdapter() {
-            override fun onBeforeResourceLoad(browser: CefBrowser?, frame: CefFrame?, request: CefRequest?): Boolean {
-                val password = serverManager.getServerPassword() ?: return false
-                val requestUrl = request?.url ?: return false
-                if (serverManager.getServerProcess()?.isAlive == true &&
-                    OpenCodeServerProtocol.shouldSendBasicAuthHeader(serverManager.getServerUrl(), requestUrl)
-                ) {
-                    request.setHeaderByName("Authorization", OpenCodeServerProtocol.buildBasicAuthHeader(password), true)
-                }
-                return false
-            }
-        }
-        private val authHandler = object : CefRequestHandlerAdapter() {
-            override fun onBeforeBrowse(
-                browser: CefBrowser?,
-                frame: CefFrame?,
-                request: CefRequest?,
-                userGesture: Boolean,
-                isRedirect: Boolean,
-            ): Boolean {
-                val requestUrl = request?.url ?: return false
-                if (!OpenCodeServerProtocol.isOpenFileLinkRequest(requestUrl)) return false
-                if (OpenCodeSettingsState.getInstance().openFileLinksInIde) {
-                    ideNavigation.openFileLinkInIde(OpenCodeServerProtocol.openFileLinkHref(requestUrl))
-                }
-                return true
-            }
-
-            override fun getResourceRequestHandler(
-                browser: CefBrowser?,
-                frame: CefFrame?,
-                request: CefRequest?,
-                isNavigation: Boolean,
-                isDownload: Boolean,
-                requestInitiator: String?,
-                disableDefaultHandling: BoolRef?,
-            ): CefResourceRequestHandler {
-                return resourceRequestHandler
-            }
-
-            override fun getAuthCredentials(
-                browser: CefBrowser?,
-                originUrl: String?,
-                isProxy: Boolean,
-                host: String?,
-                port: Int,
-                realm: String?,
-                scheme: String?,
-                callback: CefAuthCallback?,
-            ): Boolean {
-                val password = serverManager.getServerPassword() ?: return false
-                if (serverManager.getServerProcess()?.isAlive != true) return false
-                if (!OpenCodeServerProtocol.shouldHandleBasicAuthChallenge(serverManager.getServerUrl(), isProxy, host, port)) {
-                    return false
-                }
-                callback?.Continue(OpenCodeServerProtocol.BASIC_AUTH_USERNAME, password)
-                return callback != null
-            }
-        }
         private val loadHandler = object : CefLoadHandlerAdapter() {
             override fun onLoadStart(browser: CefBrowser?, frame: CefFrame?, transitionType: CefRequest.TransitionType?) {
                 if (frame?.isMain == true) {
@@ -196,7 +133,7 @@ class OpenCodeWebToolWindowContent(private val toolWindow: ToolWindow) : Disposa
                 null
             }
             retryServerButton.addActionListener { retryOpenCodeServerStart() }
-            browser.jbCefClient.addRequestHandler(authHandler, browser.cefBrowser)
+            browser.jbCefClient.addRequestHandler(requestHandler, browser.cefBrowser)
             browser.jbCefClient.addLoadHandler(loadHandler, browser.cefBrowser)
             installFileDropTransferHandler()
             updateLifecycleIndicator(serverManager.getLifecycleState())
