1095954117f160864a417ff12546502ca3347ce8
moritz <me@moritzf.de>
2026-06-27 13:23:22 +0200

    fix: avoid deprecated ToolWindowFactory bridge methods
    Annotate the Kotlin tool-window factory with @JvmDefaultWithoutCompatibility so Kotlin does not generate compatibility overrides for deprecated ToolWindowFactory default methods such as isApplicable() and isDoNotActivateOnStart().

--- src/main/kotlin/de/moritzf/opencodewebpanel/toolWindow/OpenCodeWebToolWindowFactoryImpl.kt
+++ src/main/kotlin/de/moritzf/opencodewebpanel/toolWindow/OpenCodeWebToolWindowFactoryImpl.kt
@@ -6,7 +6,9 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import kotlin.jvm.JvmDefaultWithoutCompatibility
 
+@JvmDefaultWithoutCompatibility
 class OpenCodeWebToolWindowFactoryImpl : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val toolWindowContent = OpenCodeWebToolWindowContent(toolWindow)
